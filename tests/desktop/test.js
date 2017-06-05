0f2beae2230b1d0b4d8115e15df8edf5428b3c19
Alexandr Shleyko <dusty@yandex-team.ru>
2017-06-05 21:47:53 +0300

    добавлены тесты и неуспешная авторизация

--- tests/desktop/test.js
+++ tests/desktop/test.js
@@ -1,10 +1,15 @@
 var assert = require('chai').assert;
 
 describe('Вход на сайт', function() {
-    it('Ошибка если email не заполнен', function() {
+
+    beforeEach(function() {
         return this.browser
             .url('http://dusty.dusty/')
-            .waitForVisible('.container')
+            .waitForVisible('.container');
+    });
+
+    it('Ошибка если email не заполнен', function() {
+        return this.browser
             .click('button*=Войти')
             .isVisible('.error-message')
             .then(function(visible) {
@@ -13,6 +18,33 @@ describe('Вход на сайт', function() {
             .getText('.error-message')
             .then(function(text) {
                 assert.equal(text, 'Нужно заполнить email')
+            });
+    });
+
+    it('Переход на сайт при успешной авторизации', function() {
+        return this.browser
+            .setValue('.email-input', 'admin')
+            .setValue('.password-input', 'password')
+            .click('button*=Войти')
+            .waitForVisible('.site-wrapper')
+            .getUrl()
+            .then(function(url) {
+                assert.equal(url, 'http://dusty.dusty/site.html')
+            });
+    });
+
+    it('Ошибка при неуспешной авторизации', function() {
+        return this.browser
+            .setValue('.email-input', 'user')
+            .setValue('.password-input', 'wrongpassword')
+            .click('button*=Войти')
+            .isVisible('.error-message')
+            .then(function(visible) {
+                assert.equal(visible, true)
+            })
+            .getText('.error-message')
+            .then(function(text) {
+                assert.equal(text, 'Неверные данные')
             });
     });
 });
